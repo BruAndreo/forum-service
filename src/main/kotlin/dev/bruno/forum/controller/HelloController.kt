4884b038c620af8a6444fad6ab935715f50a3016
Bruno Leiros <bruno.leiros@loggi.com>
2022-06-20 18:33:18 -0300

    Creating controllers files

--- src/main/kotlin/dev/bruno/forum/controller/HelloController.kt
+++ src/main/kotlin/dev/bruno/forum/controller/HelloController.kt
@@ -1,4 +1,4 @@
-package dev.bruno.forum
+package dev.bruno.forum.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
---
+++ src/main/kotlin/dev/bruno/forum/controller/TopicoController.kt
@@ -0,0 +1,35 @@
+package dev.bruno.forum.controller
+
+import dev.bruno.forum.model.Curso
+import dev.bruno.forum.model.Topico
+import dev.bruno.forum.model.Usuario
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/topicos")
+class TopicoController {
+
+    @GetMapping
+    fun listar(): List<Topico> {
+        val topico = Topico(
+            id = 1,
+            titulo = "Duvida Kotlin",
+            mensagem = "Variaveis no Kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+
+        return listOf(topico, topico)
+    }
+
+}
