1063e9b4f477ad5204eabf6063882b8d857c9744
Bruno Leiros <bruno.leiros@loggi.com>
2022-06-22 18:06:11 -0300

    feat: added validation in forms dto

--- src/main/kotlin/dev/bruno/forum/dto/TopicoForm.kt
+++ src/main/kotlin/dev/bruno/forum/dto/TopicoForm.kt
@@ -1,9 +1,21 @@
 package dev.bruno.forum.dto
 
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
 
 data class TopicoForm (
+    @field:NotEmpty
+    @field:Size(min = 5, max = 100)
     val titulo: String,
+
+    @field:NotEmpty
     val mensagem: String,
+
+    @field:NotNull
     val idCurso: Long,
+
+    @field:NotNull
     val idAutor: Long
 )
