002d2bb014a3a4362bb89de388f714e940cc3efc
bruandreo <brunolino2026@gmail.com>
2022-07-04 16:38:42 -0300

    feat: Created configurations to use authentication

--- src/main/kotlin/dev/bruno/forum/service/UsuarioService.kt
+++ src/main/kotlin/dev/bruno/forum/service/UsuarioService.kt
@@ -3,14 +3,21 @@ package dev.bruno.forum.service
 import dev.bruno.forum.exceptions.NotFoundException
 import dev.bruno.forum.model.Usuario
 import dev.bruno.forum.repository.UsuarioRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class UsuarioService(private val repository: UsuarioRepository) {
+class UsuarioService(private val repository: UsuarioRepository): UserDetailsService {
 
     fun buscaPorId(id: Long): Usuario {
         return repository.findById(id)
             .orElseThrow { NotFoundException("Usuário não encontrado") }
+    }
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val usuario = repository.findByEmail(username) ?: throw RuntimeException()
+        return UserDetail(usuario)
     }
 
 }
---
+++ src/main/resources/db/migration/V6__insert_password_usuario.sql
@@ -0,0 +1,5 @@
+UPDATE usuario
+SET
+    password = '$2a$12$DYZL6v8eDVXlWuFNW3ble.LJvv4Gigs6PDXTxgQsKBACbeTAWpA8e'
+WHERE
+    id = 1;
