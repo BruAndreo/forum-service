3088948145ad6fb782b5cdd5f1b8d8d10c32fdcc
Bruno Andreo <brunolino2026@gmail.com>
2022-08-19 12:31:22 -0300

    test: added container test and testing connection with postgres database

---
+++ src/test/kotlin/dev/bruno/forum/integration/TopicoRepositoryTest.kt
@@ -0,0 +1,62 @@
+package dev.bruno.forum.integration
+
+import dev.bruno.forum.dto.TopicoPorCategoriaDto
+import dev.bruno.forum.model.TopicoTest
+import dev.bruno.forum.repository.TopicoRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.domain.PageRequest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class TopicoRepositoryTest {
+
+    @Autowired
+    private lateinit var topicoRepository: TopicoRepository
+
+    private val topico = TopicoTest.build()
+    private val paginacao = PageRequest.of(0, 5)
+
+    companion object {
+        @Container
+        private val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:latest").apply {
+            withDatabaseName("testedb")
+            withUsername("dbusertest")
+            withPassword("123456")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl)
+            registry.add("spring.datasource.username", postgreSQLContainer::getUsername)
+            registry.add("spring.datasource.password", postgreSQLContainer::getPassword)
+        }
+    }
+
+    @Test
+    fun `deve gerar um relatorio`() {
+        topicoRepository.save(topico)
+        val relatorio = topicoRepository.relatorio()
+
+        assertThat(relatorio).isNotNull
+        assertThat(relatorio.first()).isExactlyInstanceOf(TopicoPorCategoriaDto::class.java)
+    }
+
+    @Test
+    fun `deve buscar topico por nome`() {
+        topicoRepository.save(topico)
+        val resultado = topicoRepository.findByCursoNome(nomeCurso = "Kotlin", paginacao = paginacao)
+
+        assertThat(resultado.totalElements).isEqualTo(1)
+    }
+}
